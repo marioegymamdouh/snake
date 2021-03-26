5e8f4b1b83bb731278283d6b77cfae4e10ad2e84
mario <maxim.moheb@smartbusinesstec.com>
2021-03-26 14:59:37 +0200

    v1.2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 const App = () => {
     const environmentSize = 20;
-    const firstSize = 10;
+    const firstSize = 19;
     const [environment, setEnvironment] = useState(() => {
         const firstEnvironment = new Array(environmentSize).fill(0).map(()=> [...new Array(environmentSize).fill(0)]);
         for (let i = 0; i < firstSize; i++) firstEnvironment[0][i] = 1;
@@ -48,12 +48,12 @@ const App = () => {
         setEnvironment(oldEnv => {
             const newEnv = [...oldEnv.map(item => [...item])]
             const head = snake.body[snake.body.length - 1];
-            newEnv[head[0]][head[1]] = 1;
             if (snake.oldTail) newEnv[snake.oldTail[0]][snake.oldTail[1]] = 0;
             else {
                 const newFood = getRandomCoordinates();
                 newEnv[newFood[0]][newFood[1]] = 9;
             }
+            newEnv[head[0]][head[1]] = 1;
             return newEnv;
         })
     }, [snake]);
@@ -97,7 +97,13 @@ const App = () => {
             const oldHeadInEnv = environment[newSnake.body[newSnake.body.length-1][0]][newSnake.body[newSnake.body.length-1][1]];
 
             if (oldHeadInEnv === 9) newSnake.oldTail = false;
-            else if (oldHeadInEnv === 1) setIsAlive(false);
+            else if (
+                oldHeadInEnv === 1 &&
+                !(
+                    newSnake.body[newSnake.body.length-1][0] === newSnake.oldTail[0] &&
+                    newSnake.body[newSnake.body.length-1][1] === newSnake.oldTail[1]
+                )
+            ) setIsAlive(false);
             else newSnake.body.shift();
 
             return newSnake;
@@ -125,7 +131,9 @@ const App = () => {
                     dim.map((cell, cellIndex) => {
                         const classes = ['cell'];
                         if (cell === 1) classes.push('snake');
-                        else if (cell === 9) classes.push('food');
+                        if (cell === 9) classes.push('food');
+                        if (dimIndex === snake.body[0][0] && cellIndex === snake.body[0][1]) classes.push('tail')
+                        if (dimIndex === snake.body[snake.body.length-1][0] && cellIndex === snake.body[snake.body.length-1][1]) classes.push('head')
                         return <div key={dimIndex + '-' + cellIndex} className={classes.join(' ')}/>
                     })
                 ).flat()}
